0afd903fe1d25c903f6d4d15dfc0b843bc88aec3
_ <53088156+636f7374@users.noreply.github.com>
2021-03-25 01:21:50 +0800

    Improve Enhanced DNS::Resolver.

--- enhanced/dns/resolver.cr
+++ enhanced/dns/resolver.cr
@@ -7,30 +7,31 @@ class DNS::Resolver
     @cloudflare
   end
 
-  def getaddrinfo(host : String, port : Int32 = 0_i32, answer_safety_first : Bool = options.addrinfo.answerSafetyFirst) : Tuple(FetchType, Array(Socket::IPAddress))
-    # This function is used as an overloadable.
+  def getaddrinfo(host : String, port : Int32 = 0_i32, answer_safety_first : Bool = options.addrinfo.answerSafetyFirst,
+                  addrinfo_override : Bool = cloudflare.try &.options.switcher.addrinfoOverride) : Tuple(FetchType, Array(Socket::IPAddress))
+    # This function is used as an overridable.
     # E.g. Cloudflare.
 
-    fetch_type, ip_addresses = tuple = getaddrinfo! host: host, port: port, answer_safety_first: answer_safety_first
+    fetch_type, ip_addresses = addrinfo_tuple = getaddrinfo! host: host, port: port, answer_safety_first: answer_safety_first
     _cloudflare = cloudflare
 
-    allowed_fetch_type = fetch_type.server? || fetch_type.caching? || fetch_type.local?
-    consistent_port = ip_addresses.all? { |ip_address| port == ip_address.port }
+    if _cloudflare && addrinfo_override
+      allowed_fetch_type = fetch_type.server? || fetch_type.caching? || fetch_type.local?
+      consistent_port = ip_addresses.all? { |ip_address| port == ip_address.port }
 
-    if _cloudflare && _cloudflare.options.switcher.addrinfoOverride && consistent_port && allowed_fetch_type
-      unless ip_addresses.empty?
-        subnet_includes = Cloudflare::Subnet.includes? ip_addresses.first
-      end
+      if consistent_port && allowed_fetch_type
+        subnet_includes = Cloudflare::Subnet.includes? ip_addresses.first unless ip_addresses.empty?
 
-      if subnet_includes
-        cloudflare_ip_addresses = _cloudflare.caching_to_tuple_ip_addresses.map do |tuple|
-          Socket::IPAddress.new address: tuple.last.address, port: port
+        if subnet_includes
+          cloudflare_ip_addresses = _cloudflare.caching_to_tuple_ip_addresses.map do |address_tuple|
+            Socket::IPAddress.new address: address_tuple.last.address, port: port
+          end
+
+          return Tuple.new FetchType::Override, cloudflare_ip_addresses unless cloudflare_ip_addresses.empty?
         end
-
-        return Tuple.new fetch_type, cloudflare_ip_addresses unless cloudflare_ip_addresses.empty?
       end
     end
 
-    tuple
+    addrinfo_tuple
   end
 end
